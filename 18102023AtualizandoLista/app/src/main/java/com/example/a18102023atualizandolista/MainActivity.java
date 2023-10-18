8034e0f9b8f371db3339c4826b4eb38c6f218892
Emily Elisabeth Neves <emily.en@aluno.ifsc.edu.br>
2023-10-18 11:53:33 -0300

    Exercício

---
+++ 18102023AtualizandoLista/app/src/main/java/com/example/a18102023atualizandolista/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.a18102023atualizandolista;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.database.sqlite.SQLiteDatabaseKt;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView lv;
+    EditText editText;
+    Button button;
+    SQLiteDatabase sqlietbd;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        lv=findViewById(R.id.listview);
+        editText=findViewById(R.id.editText);
+        button=findViewById(R.id.button);
+
+        sqlietbd=openOrCreateDatabase("meu", MODE_PRIVATE, null);
+        sqlietbd.execSQL("CREATE TABLE IF NOT EXISTS tabelinha (id INTEGER PRIMARY KEY AUTOINCREMENT,txt varchar)");
+    }
+    public void inserirNota(){
+
+    }
+    public void inserirNota(){
+
+    }
+}
